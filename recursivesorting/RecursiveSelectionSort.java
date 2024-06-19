476f60d0a85c7e78a96516e371be345345d0a1c6
ikmaz1816 <ikmaz1816@gmail.com>
2024-06-19 19:53:41 +0530

    Recursive Selection Sort

---
+++ recursivesorting/RecursiveSelectionSort.java
@@ -0,0 +1,41 @@
+package recursivesorting;
+
+public class RecursiveSelectionSort {
+	public static void main(String[] args) {
+		int[] arr= {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+		recursiveSelectionSort(arr,arr.length,0,0);
+		for(int i:arr)
+		{
+			System.out.print(i+" ");
+		}
+		System.out.println();
+	}
+
+	private static void recursiveSelectionSort(int[] arr, int r, int c, int maxIndex) 
+	{
+		if(r == 0)
+			return;
+		if(c == r)
+		{
+			swap(arr,r-1,maxIndex);
+			recursiveSelectionSort(arr,r-1,0,0);
+			return;
+		}
+		if(arr[c] > arr[maxIndex])
+		{
+			recursiveSelectionSort(arr,r,c+1,c);
+		}
+		else
+		{
+			recursiveSelectionSort(arr,r,c+1,maxIndex);
+		}
+	}
+
+
+	private static void swap(int[] arr, int j, int i) {
+		int temp = arr[j];
+		arr[j] = arr[i];
+		arr[i] = temp;
+		
+	}
+}
