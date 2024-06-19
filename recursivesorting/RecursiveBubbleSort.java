147469aa32ad2e673f3a6d6e31c3a637d01681f3
ikmaz1816 <ikmaz1816@gmail.com>
2024-06-19 19:40:16 +0530

    Recursive BubbleSort

---
+++ recursivesorting/RecursiveBubbleSort.java
@@ -0,0 +1,39 @@
+package recursivesorting;
+
+public class RecursiveBubbleSort {
+	public static void main(String[] args) {
+		int[] arr= {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+		recursiveBubbleSort(arr,arr.length-1,0);
+		for(int i:arr)
+		{
+			System.out.print(i+" ");
+		}
+		System.out.println();
+	}
+
+	
+	
+	private static void recursiveBubbleSort(int[] arr, int r, int c) {
+		if(r == 0)
+			return;
+		if(c == r)
+		{
+			recursiveBubbleSort(arr,r-1,0);
+			return;
+		}
+		if(arr[c] > arr[c+1])
+		{
+			swap(arr,c,c+1);
+		}
+		recursiveBubbleSort(arr,r,c+1);
+	}
+
+
+
+	private static void swap(int[] arr, int j, int i) {
+		int temp = arr[j];
+		arr[j] = arr[i];
+		arr[i] = temp;
+		
+	}
+}
