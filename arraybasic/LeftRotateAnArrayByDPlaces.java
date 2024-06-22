091434f1aca829f3198e61d16cb3cc1285dee885
ikmaz1816 <ikmaz1816@gmail.com>
2024-06-22 14:31:11 +0530

    Array basic question

--- arraybasic/LeftRotateAnArrayByDPlaces.java
+++ arraybasic/LeftRotateAnArrayByDPlaces.java
@@ -1,5 +1,23 @@
 package arraybasic;
 
 public class LeftRotateAnArrayByDPlaces {
+	 public void rotate(int[] nums, int k) {
+	        int length = nums.length;
+		    k%=nums.length;
+	        reverse(nums,0,length-1);
+	        reverse(nums,0,k-1);
+	        reverse(nums,k,length-1);
+	    }
 
+	    public void reverse(int[] nums,int start,int end)
+	    {
+	        while(start<end)
+	        {
+	            int temp = nums[start];
+	            nums[start] = nums[end];
+	            nums[end] = temp;
+	            start++;
+	            end--;
+	        }
+	    }
 }
