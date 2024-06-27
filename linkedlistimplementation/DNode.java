7c7c5c4926bd1761375d3ab54b96e662d9e0aa88
ikmaz1816 <ikmaz1816@gmail.com>
2024-06-27 19:32:46 +0530

    Added to use it in other packages

---
+++ linkedlistimplementation/DNode.java
@@ -0,0 +1,14 @@
+package linkedlistimplementation;
+
+public class DNode
+{
+	int data;
+	public DNode next;
+	public DNode prev;
+	public DNode(int data)
+	{
+		this.data=data;
+		next=null;
+		prev=null;
+	}
+}
