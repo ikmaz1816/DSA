57f01e89cef947464255b49d35ae767e3e60357e
ikmaz1816 <ikmaz1816@gmail.com>
2024-07-08 13:55:11 +0530

    Added data

--- bitmanipulationhard/TwoNumberAppearingOnce.java
+++ bitmanipulationhard/TwoNumberAppearingOnce.java
@@ -1,5 +1,26 @@
 package bitmanipulationhard;
 
 public class TwoNumberAppearingOnce {
-
+	 public int[] twoOddNum(int arr[], int n)
+	    {
+	        int res = 0;
+	        for(int i:arr)
+	        {
+	            res^=i;
+	        }
+	        
+	        res &= -res;
+	        int x=0,y=0;
+	        for(int i:arr)
+	        {
+	            if((res&i)!=0)
+	                x^=i;
+	            else 
+	                y^=i;
+	        }
+	        
+	        if(x>y)
+	            return new int[]{x,y};
+	        return new int[]{y,x};
+	    }
 }
