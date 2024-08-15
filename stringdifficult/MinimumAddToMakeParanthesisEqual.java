7c3acc8fecfaa9de91fc949b3aeef1bbcac6692d
ikmaz1816 <ikmaz1816@gmail.com>
2024-08-15 13:57:08 +0530

    string difficult problems

---
+++ stringdifficult/MinimumAddToMakeParanthesisEqual.java
@@ -0,0 +1,23 @@
+package stringdifficult;
+
+import java.util.Stack;
+
+public class MinimumAddToMakeParanthesisEqual {
+	 public int minAddToMakeValid(String s) {
+	        Stack<Character> st=new Stack<>();
+	        for(int i=0;i<s.length();i++)
+	        {
+	            char ch = s.charAt(i);
+	            if(ch=='(')
+	                st.push(ch);
+	            else
+	            {
+	                if(st.size()>0 && st.peek()=='(')
+	                    st.pop();
+	                else
+	                    st.push(ch);
+	            }
+	        }
+	        return st.size();
+	    }
+}
